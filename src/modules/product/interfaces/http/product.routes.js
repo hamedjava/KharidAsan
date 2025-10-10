66e1937e1300d85e1910f547955a3a76047ff9ca
hamedjava <hamedpouyanfar1987@gmail.com>
2025-10-10 18:49:58 +0330

    fix the list of products feature and all part of products is done

--- src/modules/product/interfaces/http/product.routes.js
+++ src/modules/product/interfaces/http/product.routes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const productController = require('../../../product/interfaces/controllers/product.controller.js');
 const sellerAuth = require('../../../user/seller/interfaces/middlewares/sellerAuth.middleware.js');
 
-router.post('/', sellerAuth, productController.create);
-router.put('/:id', sellerAuth, productController.update);
-router.delete('/:id', sellerAuth, productController.delete);
-router.get('/:id', productController.getById);
-router.get('/', productController.list);
+// 🔹 اول مسیرهای عمومی را تعریف کن
+router.get('/', productController.list);              // لیست همه محصولات
+router.get('/:id', productController.getById);        // دریافت محصول بر اساس id
+
+// 🔹 بعد مسیرهای نیازمند احراز هویت Seller
+router.post('/', sellerAuth, productController.create);   // ایجاد محصول
+router.put('/:id', sellerAuth, productController.update); // بروزرسانی محصول
+router.delete('/:id', sellerAuth, productController.delete); // حذف محصول
 
 module.exports = router;
